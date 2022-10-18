485f03be6269c5a107147650b370d44290f9239c
arsalan-cc <arsalan.warsi@cooperativecomputing.com>
2022-10-18 14:19:02 +0500

    QZ #time 1h #comment react application creats

---
+++ quiz-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  width: 100%;
+  height 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      Arsalan
+    </AppContainer>
+  );
+}
+
+export default App;
